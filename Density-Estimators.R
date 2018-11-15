4f6a5d00b2f45cafe13aca207c7194c53f31a838
rob <robaaleman@gmail.com>
2018-11-15 00:37:28 -0800

    Add files via upload

--- Density-Estimators.R
+++ Density-Estimators.R
@@ -62,3 +62,52 @@ CV.LeaveOneOut <- function(dens) {
 
 # Demonstration for above function
 LeaveOneOut ( rbeta(100,1,1) )
+
+"
+DENSITY
+We want to create a function to where we can select any subinterval that we may need for our distribution. We will thus call this function 
+the density function $f(x)$, where $P(a < X < b) = \int_{a}^{b} f(x) dx$. Because the density function outputs the probability that a 
+given value lies within that interval, then when we integrate our entire function $f(x)$ then it must equal 1. 
+
+INDICATOR FUNCTION DENSITY ESTIMATION
+Perhaps the most rudimentary (but non-trivial) method of measuring density takes inspiration from the histogram. We like the histogram
+because of how it is able to predict that if there are a lot of nearby values in one location, then the density will be high, and if there 
+are few values in another location, then the density at that location will be low. Drawing from this, we can construct the following 
+estimator: 
+$$ \hat{f}(x) = \frac{1}{nh} \sum \mathbf{1} (|x_i - x| < \frac{h}{2}) $$
+
+The function above looks at a given value $x$, and checks to see how many data points $x_i$ are nearby (nearby being defined as less than 
+our value $h$, which we can call our bandwidth or tolerance). The more values within the range of h, the higher the density at that point, 
+and the less values in the range of h, the lower the densty.
+
+KERNEL DENSITY ESTIMATION
+The indicator function is not bad, but suffers from a lack of 'smoothness' that tends to follow most real-world data distributions. We 
+will introduce the idea of a kernel. The last model we used was currently using is called a 'box kernel' - if we are testing a particular
+value, then our current box kernel simply returns 1 for each data point within the box and 0 for each data point out of the box. It does NOT
+take into account how close or far away each datapoint is from the value we are testing. 
+
+So instead of simply limiting ourselves to our box kernel, we will expand this to any kernel function $K$ in our density estimator: 
+$$ \hat{f}_h(x) = \frac{1}{nh} \sum K (\frac{x_i - x}{h}) $$
+
+For a kernel function to be valid, it must satisfy two things: one, $ \int K(u)du $ must equal 1 for our resulting function to be a density. 
+It must also be unimodal and symmetrical with a mean of 0. This allows us to input, say, the normal distribution, which penalizes each 
+datapoint in proportion to how far away it is from the value we are testing. 
+
+However we are still left with the same fundamental problem: how do we choose a good $h$?
+
+CROSS-VALIDATION
+Using the indicator function or maximum likelihood estimator to estimate data isn't completely useless. The reason it isn't helpful is 
+because it predicts our known data well, but says that future data is impossible. But instead what if we could tweak it to maximize future
+data, and make a 'maximum future likelihood estimator' function?
+$$L_f(x) = \prod f(x^*_j)$$ 
+
+Well, unfortunately knowing future data is impossible, otherwise we wouldn't be doing statistics. But instead, we can do something clever, 
+and 'pretend' we have future data by intentionally withholding some data from our model. This is called splitting our training data into 
+test data. Then we can run our maximum likelihood estimator on our 'known data', and see how well it predicts our 'test data' or 
+'future data'. This process of using part of our known data to find optimal parameters is called cross validation.
+
+One such method of doing cross-validation is called the leave-one-out method. It works as follows: for every value of $h$, and for every 
+value of $i$ (where $i$ is the number of datapoints in $x$), obtain the dataset $\{x_j\}, j \neq i$ (ie, dataset $x$ with every value 
+except $i$), then compute the maximum likelihood at that point which we may label $f_{-i}(x_i)$. Your output is the specified $h$ that 
+maximizes $\prod f_{-i}(x_i)$.
+"
--- Kernel-Smoothing.R
+++ Kernel-Smoothing.R
@@ -23,3 +23,28 @@ h_grid <- seq(0, 1, 0.01)
 Leave.One.Out <- function(x) {
   return (TRUE)
 }
+
+"
+KERNEL REGRESSION 
+One way to create regression models: what if we take the concept we used for a density estimator, and use it as a regression estimator? 
+Using similar methodology to KDE, we can use a weighted system to make predictions, where we look at each point, and calculate a score 
+using the proximity of nearby points, weighing close points higher and weighing far-away points lower. This weighting methodology is called 
+\textbf{LOESS}. This results in a kernel smoothing regression that looks something like this:
+$$\hat{f}(x) = \frac { \sum y_i K (\frac{x_i-x}{h}) } { \sum K (\frac{x_i-x}{h}) } $$ 
+
+Kernel regression smoothing works okay in some scenarios, but presents a lot of problems. First problem: similar to KDE, we are required to 
+select parameter $h$. If $h$ is way too small, we will overfit, or 'connect the dots' with our points. I $h$ is way too big, then we will 
+underfit, and just get a non-helpful straight line. We can at least solve this problem with cross-validation. 
+(We also need to select a kernel $K(\dot)$ but this tends to not be super important).
+
+But the bigger problem presented by kernel smoothing is its questionable effectiveness in higher dimensins. Note that we calculate weighting
+using 'closeness', and closeness is defined using distance. Distance becomes an increasingly useless metric in higher and higher dimensions.
+This is called the \textbf{curse of dimensionality}, in which the distance between points grow further and further apart. This creates a 
+'sparse' dataset which has a number of undesirable properties: for example, certain datapoints called 'hubs' become the nearest neighbour 
+of a huge percentage of the other points in the dataset. Such a machine learning model used by Pandora for example may keep recommending the
+same songs over and over and over again. 
+
+Because of this issue, it's almost always better to either use an additive model with backfitting, or to just choose a different model
+in which distance is not used to measure 'closeness'.
+
+"
